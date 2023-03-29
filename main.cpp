c5b16dad80ac9e03046fcb1a54c071c491260f2c
domross02 <ddominicross@gmail.com>
2023-03-29 01:18:36 +0000

    Mar 29, 2023, 01:18:36 UTC

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 int main(){
     Finder f;
-    vector<int> sub
+    vector<int> s1(4,6,3,4,5,5,4,5,6,7);
+    vector<int> s2(4,5,6,8,9);
+
+    for(int i=0; i<s2.size();i++){
+        cout<<s2.at(i);
+    }
 
 }

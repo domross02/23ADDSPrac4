35979dc35f4413b38b22cca88016f8eb18f4db23
domross02 <ddominicross@gmail.com>
2023-03-31 17:15:41 +1030

    first

--- Finder.cpp
+++ Finder.cpp
@@ -1,5 +1,5 @@
 #include "Finder.h"
-
+#include <iostream>
     using namespace std;
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
@@ -7,11 +7,21 @@
         vector<int> result;
 
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
+            size_t found;
+            if(result.size()>0){
+            found = s1.find(s2.substr(0, i), result.back());
+            }
+            else{
+            found = s1.find(s2.substr(0, i));
+            }
             if (found != string::npos) {
                 result.push_back(found);
             } else {
-                result.push_back(-1);
+                for(i;i<=s2.size();i++){
+                    
+                    result.push_back(-1);
+                }
+                return result;
             }
         }
         return result;
--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
-#include "Finder-1.h"
+#include "Finder.h"
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 int main(){
     Finder f;
-    vector<int> s1(4,6,3,4,5,5,4,5,6,7);
-    vector<int> s2(4,5,6,8,9);
-
-    for(int i=0; i<s2.size();i++){
-        cout<<s2.at(i);
+    string s1 = "4634554567";
+    string s2 = "45689";
+    vector<int> s3;
+    s3 = f.findSubstrings(s1,s2);
+    for(int i=0; i<s3.size();i++){
+        cout<<s3.at(i)<<endl;
     }
-
+    return 0;
 }
